2632295d013f601121991d7b1c66af614c0fbc82
Gal Tchinio <gal.tchinio@datagen.tech>
2022-12-17 23:03:18 +0200

    Add sleep

--- data_loader.py
+++ data_loader.py
@@ -98,9 +98,12 @@ def wrapper_dataset(config, args, device):
         train_loader = LoadImagesAndLabels(path="./yolov7/coco/val2017.txt", batch_size=1,
                                            stride=max(int(model.stride.max()), 32))  # todo: change to test path
         print("Getting test samples")
-        test_loader = LoadImagesAndLabels(path="./yolov7/coco/val2017.txt", batch_size=1,
+        test_loader = LoadImagesAndLabels(path="./yolov7/coco/test-dev2017.txt", batch_size=1,
                                           stride=max(int(model.stride.max()), 32))
         train_ds, test_ds = [], []
+        import time
+        print("sleeping")
+        time.sleep(4)
         for idx, data in enumerate(train_loader):
             train_x, train_label = data[0], data[1]
             batch = {'input': train_x, 'output': train_label}
