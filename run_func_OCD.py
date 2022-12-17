a8578ebde333b6d92e9d72ecec08c741c6100f25
Gal Tchinio <gal.tchinio@datagen.tech>
2022-12-18 01:30:58 +0200

    Restore forward

--- run_func_OCD.py
+++ run_func_OCD.py
@@ -81,7 +81,7 @@ train_loader, test_loader, model = wrapper_dataset(config, args, device)
 model = model.to(device)
 
 if input("Enter 't' to add graph") == "t":
-    zeros = torch.zeros(1, 3, 640, 640, device=device)
+    zeros = torch.zeros(1, 3, 40, 40, device=device)
     tb_logger.add_graph(model, zeros.to(device))
 
 if config.training.loss == 'mse':
