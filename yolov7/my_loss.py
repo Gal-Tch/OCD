ebf48d3ed255bc8c049a93afce0789c768ecd158
Gal Tchinio <gal.tchinio@datagen.tech>
2022-12-25 22:11:01 +0200

    to cuda

--- yolov7/my_loss.py
+++ yolov7/my_loss.py
@@ -100,7 +100,7 @@ if __name__ == '__main__':
     train_loader, test_loader, model = wrapper_dataset("", args, device)
     model = model.to(device)
     batch = test_loader[0]
-    predicted_labels, h = model(batch['input'].float())
+    predicted_labels, h = model(batch['input'].float().to(device))
     hx, hy = h
     hfirst = copy.deepcopy((hx.detach(), hy.detach()))
     out = copy.deepcopy(recursivley_detach(predicted_labels))
